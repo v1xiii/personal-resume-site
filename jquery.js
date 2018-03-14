a2a2e7208ce86daf499dde37bb1e11b9fa50d7a9
Lee Scholl <v1xiii@users.noreply.github.com>
2018-03-13 19:53:14 -0500

    Code cleanup
    reordered css
    changed font size to responsive

--- jquery.js
+++ jquery.js
@@ -10,19 +10,19 @@ window.onload = function() {
 	});
 
 	$(document).on("scroll", function () {
-		var pageTop = $(document).scrollTop()
-		var pageBottom = pageTop + $(window).height()
-		var tags = $(".fade2")
+		var pageTop = $(document).scrollTop();
+		var pageBottom = pageTop + $(window).height();
+		var tags = $(".fade2");
 
 		for (var i = 0; i < tags.length; i++) {
-			var tag = tags[i]
+			var tag = tags[i];
 
 			if ($(tag).position().top < pageBottom) {
-				$(tag).addClass("visible")
+				$(tag).addClass("visible");
 			} 
 			else {
-				$(tag).removeClass("visible")
+				$(tag).removeClass("visible");
 			}
 		}
-	}) 
-}
+	});
+};
